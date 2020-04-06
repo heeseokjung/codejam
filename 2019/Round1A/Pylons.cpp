fe33982ed8f65d2e073e8f8d6e2ee76e510633fe
heeseok <hycse17@gmail.com>
2020-04-06 01:25:37 -0700

    2020/04/06

---
+++ 2019/Round1A/Pylons.cpp
@@ -0,0 +1,11 @@
+#include <cstdio>
+
+int main() {
+    int TC;
+    scanf("%d", &TC);
+    for(int tc = 1; tc <= TC; ++tc) {
+        int R, C;
+        scanf("%d %d", &R, &C);
+    }
+    return 0;
+}
